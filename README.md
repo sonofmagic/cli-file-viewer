a9751b1088c8b2c30283c4d6fc30ccccbde7f007
ice breaker <1324318532@qq.com>
2021-12-27 17:37:12 +0800

    v1.0.1

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
-# npm-lib-template
+# cli-file-viewer
 
-本人用于编写的一个 `npm` 包的一个模板
+## How to use?
 
-- 使用 `tsc` 或者 `rollup` 打包
-- 使用 `jest` 设置作为单元测试
-- 使用 `eslint` 来规范代码风格，默认风格为 `standard`
-- 输出 `dist` -> `cjs`,`esm` and `.d.ts`
+### Global Install
 
-## scripts
+```bash
+yarn global add cli-file-viewer
+# then
+# if you wanna preview current dir file content with utf-8 encoding , you can exec
+cfv
 
-执行 `npm run init:rename`
- 
-作用为替换 `package.json` 中默认包含的所有名称为 `npm-lib-template` 的字段
+cfv [path]
+```
 
-默认替换为新建代码仓库的文件夹名称！
+### Use with js/ts code
+
+```js
+// esm
+import { preview } from 'cli-file-viewer'
+// cjs
+const { preview } = require('cli-file-viewer')
+
+preview(targetPath:string)
+```
+
+## Result
+
+```bash
+[File Content]
+
+[page]:17/17 [filename]:yarn.lock (prev: '← ↑' next: '→ ↓' ESC: 'ctrl + c')
+```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import readline from 'readline'
 import fs from 'fs'
 import path from 'path'
-
 const log = console.log
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,69 +9,75 @@ const rl = readline.createInterface({
 const cwd = process.cwd()
 
 const targetDirs = process.argv.slice(2)
+
 const targetPath = targetDirs[0] || cwd
-const targetStat = fs.statSync(targetPath)
 
 export interface IFileEntity {
   filename: string
   absolutePath: string
 }
 
-if (targetStat.isDirectory()) {
-  const filenameList = fs
-    .readdirSync(targetPath)
-    .reduce<IFileEntity[]>((acc, cur) => {
-      const absPath = path.resolve(targetPath, cur)
-      const stat = fs.statSync(absPath)
-      if (stat.isFile()) {
-        acc.push({
-          absolutePath: absPath,
-          filename: cur
-        })
-      }
-      return acc
-    }, [])
+export function preview(targetPath: string) {
+  const targetStat = fs.statSync(targetPath)
 
-  let idx = 0
-  const total = filenameList.length
-  function showPhoto({ filename, absolutePath }: IFileEntity, idx: number) {
-    const photo = fs.readFileSync(absolutePath, {
-      encoding: 'utf-8'
+  if (targetStat.isDirectory()) {
+    const filenameList = fs
+      .readdirSync(targetPath)
+      .reduce<IFileEntity[]>((acc, cur) => {
+        const absPath = path.resolve(targetPath, cur)
+        const stat = fs.statSync(absPath)
+        if (stat.isFile()) {
+          acc.push({
+            absolutePath: absPath,
+            filename: cur
+          })
+        }
+        return acc
+      }, [])
+
+    let idx = 0
+    const total = filenameList.length
+    function showPhoto({ filename, absolutePath }: IFileEntity, idx: number) {
+      const photo = fs.readFileSync(absolutePath, {
+        encoding: 'utf-8'
+      })
+      log('\n')
+      log(photo)
+      log(
+        `\n[page]:${
+          idx + 1
+        }/${total} [filename]:${filename} (prev: '← ↑' next: '→ ↓' ESC: 'ctrl + c')`
+      )
+    }
+
+    showPhoto(filenameList[idx], idx)
+    const onKeypress = function (_str: string, key: Record<string, string>) {
+      if (key.name === 'right' || key.name === 'down') {
+        idx++
+        if (idx >= total) {
+          idx -= total
+        }
+        showPhoto(filenameList[idx], idx)
+      }
+      if (key.name === 'left' || key.name === 'up') {
+        idx--
+        if (idx < 0) {
+          idx += total
+        }
+        showPhoto(filenameList[idx], idx)
+      }
+    }
+    process.stdin.on('keypress', onKeypress)
+    rl.on('close', () => {
+      process.stdin.off('keypress', onKeypress)
     })
-    log('\n')
-    log(photo)
+  } else if (targetStat.isFile()) {
     log(
-      `\n[page]:${
-        idx + 1
-      }/${total} [filename]:${filename} (prev: '← ↑' next: '→ ↓' ESC: 'ctrl + c')`
+      fs.readFileSync(targetPath, {
+        encoding: 'utf-8'
+      })
     )
   }
-
-  showPhoto(filenameList[idx], idx)
-  const onKeypress = function (_str: string, key: Record<string, string>) {
-    if (key.name === 'right' || key.name === 'down') {
-      idx++
-      if (idx >= total) {
-        idx -= total
-      }
-      showPhoto(filenameList[idx], idx)
-    }
-    if (key.name === 'left' || key.name === 'up') {
-      idx--
-      if (idx < 0) {
-        idx += total
-      }
-      showPhoto(filenameList[idx], idx)
-    }
-  }
-  process.stdin.on('keypress', onKeypress)
-  rl.on('close', () => {
-    process.stdin.off('keypress', onKeypress)
-  })
-} else if (targetStat.isFile()) {
-  log(
-    fs.readFileSync(targetPath, {
-      encoding: 'utf-8'
-    })
-  )
 }
+
+preview(targetPath)
